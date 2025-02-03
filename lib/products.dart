4da2b9d1a3b7a4d3ad742ac3134b6ce14c1804ce
Abdihakim639 <abdihakimibrahim996@gmail.com>
2025-02-03 21:30:17 +0300

    Update products.dart

--- lib/products.dart
+++ lib/products.dart
@@ -9,3 +9,13 @@ class ProductsScreen extends StatelessWidget {
         backgroundColor: Colors.purple,
         title: Text('Products', style: TextStyle(color: Colors.white)),
       ),
+      body: ListView(
+        padding: EdgeInsets.all(16.0),
+        children: [
+          _buildProductItem(context, 'Beef Burger', 'assets/burger.png', 20),
+          _buildProductItem(context, 'Noodles', 'assets/noodles.png', 9),
+        ],
+      ),
+    );
+  }
+
